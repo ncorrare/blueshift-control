5cca65fe4bc13f767954a4afeac0aa99d08fca02
Nicolas Corrarello <ncorrare@gmail.com>
2016-10-12 12:53:50 +0100

    Fixed templates

--- site/profile/manifests/kubeminion.pp
+++ site/profile/manifests/kubeminion.pp
@@ -9,21 +9,21 @@ class profile::kubeminion (
 
   file { '/etc/sysconfig/flanneld':
     ensure  => file,
-    content => template('profile/flanneld.erb', { 'kubemaster' => $kubemaster }),
+    content => template('profile/flanneld.erb'),
     require => Package['flannel'],
     notify  => Service['flanneld'],
   }
 
   file { '/etc/kubernetes/config':
     ensure  => file,
-    content => template('profile/config.erb', { 'kubemaster' => $kubemaster }),
+    content => template('profile/config.erb'),
     require => Package['kubernetes'],
     notify  => Service['kubelet','kube-proxy'],
   }
 
   file { '/etc/kubernetes/kubelet':
     ensure  => file,
-    content => template('profile/kubelet.erb', { 'kubemaster' => $kubemaster }),
+    content => template('profile/kubelet.erb'),
     require => Package['kubernetes'],
     notify  => Service['kubelet'],
   }
